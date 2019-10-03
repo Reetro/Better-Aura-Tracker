0fc9add5c21d556f18332e5ea02ff4c195a635b5
Reetro <thegamerboy7@gmail.com>
2019-10-03 10:05:39 -0600

    Added in Ace libraries

--- timer.lua
+++ timer.lua
@@ -1,8 +1,3 @@
--[[ 
-	Will allow us to create timers to track buff and debuff durations.
-	Timers can be set to icon or timer bar mode.
---]]
-
 ----------------------
 -- Namespaces
 ----------------------
@@ -14,24 +9,10 @@ local Timer = core.Timer;
 ----------------------
 -- Timer functions
 ----------------------
---[[
-	Should return an instance of a Timer (using metatables).
-	A "static" function.
---]]
-function Timer:Create()
+
+-- Add Timer to Screen
+function createTimer() 
 	
 end
 
---[[
-	Should update the duration of the timer using events.
---]]
-function Timer:Update()
-	
-end
 
---[[
-	Destroy the timer after it has expired (recylce it).
---]]
-function Timer:Destroy()
-
-end 
