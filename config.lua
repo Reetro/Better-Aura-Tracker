32c05cb2381f3c1402c0d17e0406c0ec1e9202e8
Reetro <thegamerboy7@gmail.com>
2019-10-07 12:04:19 -0600

    More Work on Options menu

--- config.lua
+++ config.lua
@@ -30,18 +30,25 @@ function Config:GetThemeColor()
 	return c.r, c.g, c.b, c.hex;
 end
 
+local SimpleRound = function(val,valStep)
+    return floor(val/valStep)*valStep
+  end
+
+
 function AddText(frame, point, xoff,yoff,text,size)
 	local t = frame.panel:CreateFontString(nil, "ARTWORK", "GameFontNormalLarge")
 	t:SetPoint(point, xoff, yoff)
 	t:SetText(text)
-	t:SetFont("Fonts\\FRIZQT__.TTF", size, "OUTLINE, MONOCHROME")
+	t:SetFont("Fonts\\MORPHEUS.ttf", size, "OUTLINE, MONOCHROME")
+	return t
 end
 
 function AddSubText(frame, point, xoff,yoff,text,size)
 	local t = frame.panel:CreateFontString(nil, "ARTWORK", "GameFontHighlightSmall")
 	t:SetPoint(point, xoff, yoff)
 	t:SetText(text)
-	t:SetFont("Fonts\\FRIZQT__.TTF", size, "OUTLINE, MONOCHROME")
+	t:SetFont("Fonts\\MORPHEUS.ttf", size, "OUTLINE, MONOCHROME")
+	return t
 end
 
 function CreateButton(point, relativeFrame, relativePoint, xoffset, yOffset, width, height, text)
@@ -54,21 +61,66 @@ function CreateButton(point, relativeFrame, relativePoint, xoffset, yOffset, wid
 	return btn;
 end
 
+function CreateSlider(name, title, point, relativeFrame, relativePoint, xoffset, yOffset, min, max, valuestep, start)
+	local s = CreateFrame("SLIDER", name, relativeFrame, "OptionsSliderTemplate")
+	local editbox = CreateFrame("EditBox",  "$parentEditBox", s, "InputBoxTemplate")
+	s.text = _G[name.."Text"]
+	s.text:SetText(title)
+	s.textLow = _G[name.."Low"]
+    s.textHigh = _G[name.."High"]
+    s.textLow:SetText(floor(min))
+	s.textHigh:SetText(floor(max))
+	s.textLow:SetTextColor(0.4,0.4,0.4)
+    s.textHigh:SetTextColor(0.4,0.4,0.4)
+	s:SetPoint(point, relativeFrame, relativePoint, xoffset, yOffset)
+	s:SetMinMaxValues(min,max)
+	s:SetValue(start)
+	s:SetValueStep(valuestep)
+	s:SetObeyStepOnDrag(true)	
+	editbox:SetSize(50,30)
+    editbox:ClearAllPoints()
+    editbox:SetPoint("LEFT", s, "RIGHT", 15, 0)
+    editbox:SetText(s:GetValue())
+    editbox:SetAutoFocus(false)
+    s:SetScript("OnValueChanged", function(self,value)
+      self.editbox:SetText(SimpleRound (value,valuestep))
+    end)
+    editbox:SetScript("OnTextChanged", function(self)
+      local val = self:GetText()
+      if tonumber(val) then
+         self:GetParent():SetValue(val)
+      end
+    end)
+    editbox:SetScript("OnEnterPressed", function(self)
+      local val = self:GetText()
+      if tonumber(val) then
+         self:GetParent():SetValue(val)
+         self:ClearFocus()
+      end
+    end)
+    s.editbox = editbox
+    return slider
+end
+
 
 function Config:CreateMenu()
 	-- Register in the Interface Addon Options GUI	
 	BetterAuraTrackerPanel = {};
 	BetterAuraTrackerPanel.panel = CreateFrame( "Frame", "BetterAuraTrackerPanel", UIParent );
 	-- Title Text
-	local title = AddText(BetterAuraTrackerPanel, "TOPLEFT", 16, -16, "BetterAuraTracker Options", 30)
+	BetterAuraTrackerPanel.panel.title = AddText(BetterAuraTrackerPanel, "TOPLEFT", 16, -16, "BetterAuraTracker Options", 30)
 	-- Unlock Button 
-	local Unlock = CreateButton("TOPLEFT", BetterAuraTrackerPanel.panel, "TOPLEFT", 20, -60, 140, 50, "Unlock")
+	BetterAuraTrackerPanel.panel.Unlock = CreateButton("TOPLEFT", BetterAuraTrackerPanel.panel, "TOPLEFT", 20, -60, 140, 40, "Unlock")
 	-- Reset Button 
-	-- Unlock Button 
-	local Unlock = CreateButton("TOPLEFT", BetterAuraTrackerPanel.panel, "TOPLEFT", 20, -60, 140, 50, "Unlock")
+	BetterAuraTrackerPanel.panel.Reset = CreateButton("TOP", BetterAuraTrackerPanel.panel.Unlock, "TOP", 150, 0, 140, 40, "Reset")
+	-- Lock Button
+	BetterAuraTrackerPanel.panel.Lock = CreateButton("TOP", BetterAuraTrackerPanel.panel.Reset, "TOP", 150, 0, 140, 40, "Lock")
+	-- Buff Sub Text 
+	BetterAuraTrackerPanel.panel.BuffSub = AddSubText(BetterAuraTrackerPanel, "TOPLEFT", 20, -180, "Buff Options", 18)
+	-- Buff Scale Slider
+	BetterAuraTrackerPanel.panel.BuffSlider = CreateSlider("BuffScaleSlider", "Buff Scale", "TOPLEFT", BetterAuraTrackerPanel.panel, "TOPLEFT", 20, -240, 1, 10, 1, 1)
+
 	
-
-
 	-- Set the name for the Category for the Options Panel
 	BetterAuraTrackerPanel.panel.name = "BetterAuraTracker";
 
